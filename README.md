a31bc7fa36f7a50fd360c130b5188cdcd20f454e
Alejandro <usercall.hm@gmail.com>
2018-03-01 23:55:01 -0500

    add libs
    add chars valid

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ccop
+
+# Copilador
+
+
+| Expresión infija 	| Expresión prefija 	| Expresión sufija |
+| A + B 	          | + A B               |	A B +            |
+| A + B * C 	      | + A * B C 	        | A B C * +        |
+
+
+## INTEGRANTES
+- [Diego Alejandro Hernandez](https://github.com/USERCALL)  -> 20141020271
+- [Dwan  Felipe Veloza Paez](https://github.com/Dwan13)     -> 20142020098
--- lectura.py
+++ lectura.py
@@ -1,9 +1,18 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
 
+## listas por comprension
 
-## listas pro compresnion
+
 print("leyendo")
-
 lista = [y.split(" ")  for y in [x.strip("=\n") for x in open("operacion.txt","r")]]
-print lista
+print("Datos Almacenados")
+print(lista)
+
+charAcepted = "1234567890+-*/"
+
+for x in lista:
+    for y in x:
+        if y not in charAcepted:
+            print type(y)
+            print( y + " : is not accepted")
